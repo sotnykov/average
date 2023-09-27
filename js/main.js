78136f3750ec07c24235169a7ac24a12a1a64b4f
Andrii Sotnykov <andrew.sotnikov1437@gmail.com>
2023-09-27 18:04:58 +0200

    added average calculator

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Write the first number:');
+
+let num2 = prompt('Write the second number:');
+
+let num3 = prompt('Write the third number:');
+
+const average = (+num1 + +num2 + +num3) / 3;
+
+alert(`Average number is ${average}!`);
