2a7c9e76972491cf93f593881972d6b9f1cb5376
Andrii Sotnykov <andrew.sotnikov1437@gmail.com>
2023-10-07 15:03:10 +0200

    added NaN checker and the result is round number

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
-let num1 = prompt('Write the first number:');
+let num1;
 
-let num2 = prompt('Write the second number:');
+do {
+  num1 = prompt('Write the first number:');
 
-let num3 = prompt('Write the third number:');
+  if (isNaN(num1)) {
+    alert(`${num1} is not a number!`);
+  }
+} while (isNaN(num1));
+
+let num2;
+
+do {
+  num2 = prompt('Write the second number:');
+
+  if (isNaN(num2)) {
+    alert(`${num2} is not a number!`);
+  }
+} while (isNaN(num2));
+
+let num3;
+
+do {
+  num3 = prompt('Write the third number:');
+
+  if (isNaN(num3)) {
+    alert(`${num3} is not a number`);
+  }
+} while (isNaN(num3));
 
 const average = (+num1 + +num2 + +num3) / 3;
 
-alert(`Average number is ${average}!`);
+alert(`Average number is ${Math.round(average)}!`);
